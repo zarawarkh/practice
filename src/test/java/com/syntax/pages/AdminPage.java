40a43d55316a87c61e4148588d8551f51a828508
zarawarkh <zarawar003@gmail.com>
2022-01-26 23:26:11 -0500

    updated

--- src/test/java/com/syntax/pages/AdminPage.java
+++ src/test/java/com/syntax/pages/AdminPage.java
@@ -2,11 +2,8 @@ package com.syntax.pages;
 
 public class AdminPage {
 
-<<<<<<< HEAD
-	public static void str2() {
-=======
 	public static void str (){
->>>>>>> ff13f0bb6ae3a54e1123fc517694578e672669b6
+
 		System.out.println("print statement");
 	}
 }
