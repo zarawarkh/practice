7558bce0742a2cc284fb96e5555fe700fd7cf4ab
zarawarkh <zarawar003@gmail.com>
2022-01-26 23:42:37 -0500

    Leave test cased added

--- src/test/java/com/syntax/pages/LeavePage.java
+++ src/test/java/com/syntax/pages/LeavePage.java
@@ -5,4 +5,8 @@ public class LeavePage {
     public static void method1(){
         System.out.println("print statement");
     }
+    public static void method2(){
+        System.out.println("print statement2");
+        System.out.println("print statement2");
+    }
 }
