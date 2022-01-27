c6221329963340d6ad1d2f84103aea76ebfcf551
zarawarkh <zarawar003@gmail.com>
2022-01-26 21:42:12 -0500

    code added

---
+++ src/test/java/com/syntax/stepDefinitions/ListenersTestNG.java
@@ -0,0 +1,54 @@
+/*
+package com.syntax.stepDefinitions;
+
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+public class ListenersTestNG implements ITestListener {
+
+	@Override
+	public void onTestStart(ITestResult result) {
+		System.out.println("onStart method started");
+		
+	}
+
+	@Override
+	public void onTestSuccess(ITestResult result) {
+		System.out.println("onTestSuccess Method");
+		
+	}
+
+	@Override
+	public void onTestFailure(ITestResult result) {
+		System.out.println("onTestFailure Method");
+		
+	}
+
+	@Override
+	public void onTestSkipped(ITestResult result) {
+		System.out.println("onTestSkipped Method");
+		
+	}
+
+	@Override
+	public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
+		System.out.println("onTestFailedButWithinSuccessPercentage");
+		
+	}
+
+	@Override
+	public void onStart(ITestContext context) {
+		System.out.println("New Test Started");
+		
+	}
+
+	@Override
+	public void onFinish(ITestContext context) {
+		System.out.println("onFinish method started");
+		
+	}
+
+	
+}
+*/
