bfc2a6c0e42e6c50b343a93997e82d5f4ba61497
zarawarkh <zarawar003@gmail.com>
2022-01-26 23:05:23 -0500

    last night

--- src/test/java/com/syntax/pages/AdminPage.java
+++ src/test/java/com/syntax/pages/AdminPage.java
@@ -2,18 +2,5 @@ package com.syntax.pages;
 
 public class AdminPage {
 
-	public static void Print () {
-
-		System.out.println("Print statement");
-		System.out.println("Print statement");
-		System.out.println("Print statement");
-		System.out.println("Print statement");
-
-	}
 	
-	public static void Print2 () {
-
-		System.out.println("Print statement2");
-
-	}
 }
